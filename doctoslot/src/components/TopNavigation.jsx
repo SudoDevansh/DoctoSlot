ff75c815ce3cf5a0deb7b769fbe046af0479a3e7
Devansh Chauhan <devanshc603@gmail.com>
2026-05-07 00:31:25 +0530

    top-navbar

---
+++ doctoslot/src/components/TopNavigation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TopNavigation() {
+    return (
+        <div className="top-nav">
+            <input type="text" placeholder="Search..." className="search-bar" />
+            <div className="user-profile">
+                <span>Dr. Sharma</span>
+            </div>
+        </div>
+    )
+}
